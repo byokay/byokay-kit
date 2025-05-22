56606507bb2194a0237c98c77fdd9ac649ddafa5
m <13090422+marciob@users.noreply.github.com>
2025-05-22 00:43:19 -0300

    adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // This will scan all relevant files in your src folder
+  ],
+  theme: {
+    extend: {}, // You can customize Tailwind's default theme here
+  },
+  plugins: [],
+};
